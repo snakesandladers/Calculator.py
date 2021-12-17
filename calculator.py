a6c7f68fef35e0e41ef18ac31dd57f41b1215075
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-17 14:52:46 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(x,y):
+ #this adds the given 2 numbers
+ return x + y 
+def substract(x,y):
+     return x - y
+def multiplication(x,y):
+     return x * y
+def division(x,y):     
+    return  x / y 
+print ("select operation.")
+print("1.add")
+print("1.substract")
+
+print("1.multiply")
+
+print("1.division")
+choice=input("enter choice 1/2/3/4/5:")
+num1=int(input("enter first number :"))
+num2=int(input("enter second number:"))
+if choice=='1':
+    print(num1,"+",num2,"=",add(num1,num2))
+elif choice=='2':
+     print(num1,"-", num2,"=",substract(num1,num2))
+
+elif choice=='3':
+    print(num1,"*",num2,"=",multiplication(num1,num2))
+elif choice=='4':
+    print(num1,"/",num2,"=",division(num1,num2))
+else:
+    print("invalid input")
